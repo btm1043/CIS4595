a5217d6aceb71939f4473de1ffa72ad3325edb7f
btm1043 <39682210+btm1043@users.noreply.github.com>
2020-11-05 08:44:23 -0600

    Start of Webrtc
    accessing /videoroom/* will pull chatroom with localvideo stream.

---
+++ public/javascripts/videoroom.js
@@ -0,0 +1,46 @@
+const socket = io();
+
+const inboxPeople = document.getElementById("activeUsers");
+let userName = "";
+
+const newUserConnected = (user) => {
+  userName = user || `User${Math.floor(Math.random() * 1000000)}`;
+  
+  socket.emit("new user", userName+","+(document.location.pathname).split("/")[2]);
+  $('#ausers').append($('<li>').html(userName));
+  addToUsersBox(userName);
+};
+
+const addToUsersBox = (userName) => {
+  if (!!document.querySelector(`.${userName}-userlist`)) {
+    return;
+  }
+  const userBox = `
+    <div class="chat_ib ${userName}-userlist">
+      <h5>${userName}</h5>
+    </div>
+  `;
+  inboxPeople.innerHTML += userBox;
+};
+
+$('#chatForm').submit(function(e){
+		e.preventDefault();
+		socket.emit('chat_message',$('#txt').val());
+		$('#txt').val('');
+		return false;
+});
+
+socket.on("new user", function (data) {
+  data.map((user) => addToUsersBox(user));
+});
+
+socket.on("user disconnected", function (userName) {
+  document.querySelector(`.${userName}-userlist`).remove();
+});
+
+socket.on('chat_message', function(msg){
+	$('#messages').append($('<li>').html(msg));
+	var mdiv=document.getElementById("chatmessages");
+	mdiv.scrollTop=mdiv.scrollHeight;
+});
+
